3b34a5d191734051cc2f0e0fe196cede16017be9
samigrmn <86789068+samigrmn@users.noreply.github.com>
2022-06-26 15:10:13 +0300

    LavaFloor
    Project made with Unity

---
+++ PatikaLavaFloor/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float hiz = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zYonu = Input.GetAxis("Vertical") * hiz;
+        float xYonu = Input.GetAxis("Horizontal") * hiz;
+        zYonu *= Time.deltaTime;
+        xYonu *= Time.deltaTime;
+        transform.Translate(xYonu, 0, zYonu);
+
+
+
+
+
+
+
+
+     
+    }
+}
